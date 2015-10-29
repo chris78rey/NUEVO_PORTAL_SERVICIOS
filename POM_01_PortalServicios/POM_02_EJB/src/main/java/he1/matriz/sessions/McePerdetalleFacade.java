d42c3138ba1b49950002b611e9514a98c4afa3f2
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-10-29 14:53:26 -0500

    funca ya sentinel nuevamente

---
+++ POM_01_PortalServicios/POM_02_EJB/src/main/java/he1/matriz/sessions/McePerdetalleFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package he1.matriz.sessions;
+
+import he1.matriz.entities.McePerdetalle;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author luis_guanoluiza
+ */
+@Stateless
+public class McePerdetalleFacade extends AbstractFacade<McePerdetalle> {
+    @PersistenceContext(unitName = "EJB_HE1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public McePerdetalleFacade() {
+        super(McePerdetalle.class);
+    }
+    
+}
---
+++ POM_01_PortalServicios/POM_02_EJB/src/main/java/he1/sis/sessions/DiuresisFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package he1.sis.sessions;
+
+
+import he1.sis.entities.Diuresis;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author luis_guanoluiza
+ */
+@Stateless
+public class DiuresisFacade extends AbstractFacade<Diuresis> {
+    @PersistenceContext(unitName = "EJB_HE1PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public DiuresisFacade() {
+        super(Diuresis.class);
+    }
+    
+}
