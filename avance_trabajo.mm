3c637a9f714718d08ba4ee78d559928283478fbc
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-10 12:04:19 -0500

    El pom se modifica y

---
+++ avance_trabajo.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1447159522865" ID="ID_268672206" MODIFIED="1447159522865" TEXT="Nuevo Mapa"/>
+</map>
