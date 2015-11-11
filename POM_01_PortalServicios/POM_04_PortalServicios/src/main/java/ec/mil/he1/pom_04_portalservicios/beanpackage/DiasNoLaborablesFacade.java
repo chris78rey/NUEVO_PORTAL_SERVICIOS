6e6c54d8590189f31c659993d41e8da4c3af30e7
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 11:39:46 -0500

    SE AGREGA ELSCHEMA

---
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/DiasNoLaborablesFacade.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.mil.he1.pom_04_portalservicios.beanpackage;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author christian_ruiz
+ */
+public class DiasNoLaborablesFacade {
+    @PersistenceContext(unitName = "PU-WEB-PORTAL")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
