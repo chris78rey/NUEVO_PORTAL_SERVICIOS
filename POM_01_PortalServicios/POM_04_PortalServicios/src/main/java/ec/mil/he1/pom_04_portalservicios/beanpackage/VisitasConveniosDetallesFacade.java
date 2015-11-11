897a2a7767dd95a2f5a700958ff1571036f31388
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 12:02:35 -0500

    SE AGREGA ELSCHEMA

---
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/VisitasConveniosDetallesFacade.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.mil.he1.pom_04_portalservicios.beanpackage;
+
+/**
+ *
+ * @author christian_ruiz
+ */
+public class VisitasConveniosDetallesFacade {
+    
+}
