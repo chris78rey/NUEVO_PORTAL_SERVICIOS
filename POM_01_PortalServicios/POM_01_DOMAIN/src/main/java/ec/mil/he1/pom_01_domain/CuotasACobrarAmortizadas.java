0506fcf9ba74fd24dfd0806d73546708a9c391a8
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 10:00:02 -0500

    SE AGREGA ELSCHEMA

--- POM_01_PortalServicios/POM_01_DOMAIN/src/main/java/ec/mil/he1/pom_01_domain/CuotasACobrarAmortizadas.java
+++ POM_01_PortalServicios/POM_01_DOMAIN/src/main/java/ec/mil/he1/pom_01_domain/CuotasACobrarAmortizadas.java
@@ -5,10 +5,246 @@
  */
 package ec.mil.he1.pom_01_domain;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author christian_ruiz
  */
-public class CuotasACobrarAmortizadas {
+@Entity
+@Table(name = "CUOTAS_A_COBRAR_AMORTIZADAS")
+@NamedQueries({
+    @NamedQuery(name = "CuotasACobrarAmortizadas.findAll", query = "SELECT c FROM CuotasACobrarAmortizadas c")})
+public class CuotasACobrarAmortizadas implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "NUMERO")
+    private Long numero;
+    @Column(name = "NUMERO_CUOTA")
+    private Short numeroCuota;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "VALOR")
+    private BigDecimal valor;
+    @Column(name = "FECHA_CREACION")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCreacion;
+    @Column(name = "FECHA_VENCIMIENTO")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaVencimiento;
+    @Column(name = "USERNAME")
+    private String username;
+    @Column(name = "FECHA_PLAZO")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaPlazo;
+    @Column(name = "INTERES")
+    private BigDecimal interes;
+    @Column(name = "INTERES_PORCENTAJE")
+    private BigDecimal interesPorcentaje;
+    @Column(name = "MORA")
+    private BigDecimal mora;
+    @Column(name = "MORA_PORCENTAJE")
+    private BigDecimal moraPorcentaje;
+    @Column(name = "SALDO")
+    private BigDecimal saldo;
+    @Column(name = "SALDO_CUOTA")
+    private BigDecimal saldoCuota;
+    @Column(name = "CTACBR_NUMERO")
+    private Long ctacbrNumero;
+    @Column(name = "MDOPGO_NUMERO")
+    private Short mdopgoNumero;
+    @Column(name = "PRS_CODIGO")
+    private String prsCodigo;
+    @Column(name = "CJA_CODIGO")
+    private String cjaCodigo;
+    @Column(name = "OBSERVACION")
+    private String observacion;
+
+    public CuotasACobrarAmortizadas() {
+    }
+
+    public CuotasACobrarAmortizadas(Long numero) {
+        this.numero = numero;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Short getNumeroCuota() {
+        return numeroCuota;
+    }
+
+    public void setNumeroCuota(Short numeroCuota) {
+        this.numeroCuota = numeroCuota;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getFechaPlazo() {
+        return fechaPlazo;
+    }
+
+    public void setFechaPlazo(Date fechaPlazo) {
+        this.fechaPlazo = fechaPlazo;
+    }
+
+    public BigDecimal getInteres() {
+        return interes;
+    }
+
+    public void setInteres(BigDecimal interes) {
+        this.interes = interes;
+    }
+
+    public BigDecimal getInteresPorcentaje() {
+        return interesPorcentaje;
+    }
+
+    public void setInteresPorcentaje(BigDecimal interesPorcentaje) {
+        this.interesPorcentaje = interesPorcentaje;
+    }
+
+    public BigDecimal getMora() {
+        return mora;
+    }
+
+    public void setMora(BigDecimal mora) {
+        this.mora = mora;
+    }
+
+    public BigDecimal getMoraPorcentaje() {
+        return moraPorcentaje;
+    }
+
+    public void setMoraPorcentaje(BigDecimal moraPorcentaje) {
+        this.moraPorcentaje = moraPorcentaje;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public BigDecimal getSaldoCuota() {
+        return saldoCuota;
+    }
+
+    public void setSaldoCuota(BigDecimal saldoCuota) {
+        this.saldoCuota = saldoCuota;
+    }
+
+    public Long getCtacbrNumero() {
+        return ctacbrNumero;
+    }
+
+    public void setCtacbrNumero(Long ctacbrNumero) {
+        this.ctacbrNumero = ctacbrNumero;
+    }
+
+    public Short getMdopgoNumero() {
+        return mdopgoNumero;
+    }
+
+    public void setMdopgoNumero(Short mdopgoNumero) {
+        this.mdopgoNumero = mdopgoNumero;
+    }
+
+    public String getPrsCodigo() {
+        return prsCodigo;
+    }
+
+    public void setPrsCodigo(String prsCodigo) {
+        this.prsCodigo = prsCodigo;
+    }
+
+    public String getCjaCodigo() {
+        return cjaCodigo;
+    }
+
+    public void setCjaCodigo(String cjaCodigo) {
+        this.cjaCodigo = cjaCodigo;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (numero != null ? numero.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CuotasACobrarAmortizadas)) {
+            return false;
+        }
+        CuotasACobrarAmortizadas other = (CuotasACobrarAmortizadas) object;
+        if ((this.numero == null && other.numero != null) || (this.numero != null && !this.numero.equals(other.numero))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ec.mil.he1.pom_01_domain.CuotasACobrarAmortizadas[ numero=" + numero + " ]";
+    }
     
 }
