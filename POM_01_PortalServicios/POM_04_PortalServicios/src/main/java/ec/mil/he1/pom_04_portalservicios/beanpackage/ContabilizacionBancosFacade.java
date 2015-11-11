4edf57efcb8a0d50a39c68251eb06c61ddb3162e
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 11:35:32 -0500

    SE AGREGA ELSCHEMA

---
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/ContabilizacionBancosFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.mil.he1.pom_04_portalservicios.beanpackage;
+
+import ec.mil.he1.pom_01_domain.ContabilizacionBancos;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author christian_ruiz
+ */
+@Stateless
+public class ContabilizacionBancosFacade extends AbstractFacade<ContabilizacionBancos> {
+    @PersistenceContext(unitName = "PU-WEB-PORTAL")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ContabilizacionBancosFacade() {
+        super(ContabilizacionBancos.class);
+    }
+    
+}
