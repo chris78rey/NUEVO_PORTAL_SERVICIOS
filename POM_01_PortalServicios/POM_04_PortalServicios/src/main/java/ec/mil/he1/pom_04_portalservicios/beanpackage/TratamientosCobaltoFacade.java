eb757f51e2f1ec4fade421467d72ac1bac423433
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 12:00:48 -0500

    SE AGREGA ELSCHEMA

--- POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/TratamientosCobaltoFacade.java
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/TratamientosCobaltoFacade.java
@@ -5,7 +5,7 @@
  */
 package ec.mil.he1.pom_04_portalservicios.beanpackage;
 
-import ec.mil.he1.pom_01_domain.Transacciones;
+import ec.mil.he1.pom_01_domain.TratamientosCobalto;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,7 +15,7 @@ import javax.persistence.PersistenceContext;
  * @author christian_ruiz
  */
 @Stateless
-public class TransaccionesFacade extends AbstractFacade<Transacciones> {
+public class TratamientosCobaltoFacade extends AbstractFacade<TratamientosCobalto> {
     @PersistenceContext(unitName = "PU-WEB-PORTAL")
     private EntityManager em;
 
@@ -24,8 +24,8 @@ public class TransaccionesFacade extends AbstractFacade<Transacciones> {
         return em;
     }
 
-    public TransaccionesFacade() {
-        super(Transacciones.class);
+    public TratamientosCobaltoFacade() {
+        super(TratamientosCobalto.class);
     }
     
 }
