6e737bdc02e2039d9015cd286c056d7fc28ffed6
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 11:59:09 -0500

    SE AGREGA ELSCHEMA

--- POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/TiposDeAlergiasFacade.java
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/TiposDeAlergiasFacade.java
@@ -5,7 +5,7 @@
  */
 package ec.mil.he1.pom_04_portalservicios.beanpackage;
 
-import ec.mil.he1.pom_01_domain.TecnicasPatologicas;
+import ec.mil.he1.pom_01_domain.TiposDeAlergias;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,7 +15,7 @@ import javax.persistence.PersistenceContext;
  * @author christian_ruiz
  */
 @Stateless
-public class TecnicasPatologicasFacade extends AbstractFacade<TecnicasPatologicas> {
+public class TiposDeAlergiasFacade extends AbstractFacade<TiposDeAlergias> {
     @PersistenceContext(unitName = "PU-WEB-PORTAL")
     private EntityManager em;
 
@@ -24,8 +24,8 @@ public class TecnicasPatologicasFacade extends AbstractFacade<TecnicasPatologica
         return em;
     }
 
-    public TecnicasPatologicasFacade() {
-        super(TecnicasPatologicas.class);
+    public TiposDeAlergiasFacade() {
+        super(TiposDeAlergias.class);
     }
     
 }
