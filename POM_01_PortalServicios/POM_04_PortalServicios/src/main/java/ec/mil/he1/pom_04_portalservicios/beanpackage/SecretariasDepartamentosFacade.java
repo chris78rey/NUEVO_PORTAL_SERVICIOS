5f634d59b672ee359d1013d7e7094d713ccef70c
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 11:54:39 -0500

    SE AGREGA ELSCHEMA

--- POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/SecretariasDepartamentosFacade.java
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/SecretariasDepartamentosFacade.java
@@ -5,6 +5,8 @@
  */
 package ec.mil.he1.pom_04_portalservicios.beanpackage;
 
+import ec.mil.he1.pom_01_domain.SecretariasDepartamentos;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -12,13 +14,18 @@ import javax.persistence.PersistenceContext;
  *
  * @author christian_ruiz
  */
-public class RolacumuladoanualFacade {
+@Stateless
+public class SecretariasDepartamentosFacade extends AbstractFacade<SecretariasDepartamentos> {
     @PersistenceContext(unitName = "PU-WEB-PORTAL")
     private EntityManager em;
 
     @Override
     protected EntityManager getEntityManager() {
         return em;
+    }
+
+    public SecretariasDepartamentosFacade() {
+        super(SecretariasDepartamentos.class);
     }
     
 }
