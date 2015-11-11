fa71cff3b082838093ca1eb164b439ae379d1944
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 09:45:55 -0500

    SE AGREGA ELSCHEMA

---
+++ POM_01_PortalServicios/POM_01_DOMAIN/src/main/java/ec/mil/he1/pom_01_domain/RegistrosObstetriciaPK.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.mil.he1.pom_01_domain;
+
+/**
+ *
+ * @author christian_ruiz
+ */
+public class RegistrosObstetriciaPK {
+    
+}
