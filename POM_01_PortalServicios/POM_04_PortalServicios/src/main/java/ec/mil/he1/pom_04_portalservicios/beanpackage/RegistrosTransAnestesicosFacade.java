8672f786f8509d8e4bdd62c1c15e4b62667238fe
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 11:52:23 -0500

    SE AGREGA ELSCHEMA

--- POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/RegistrosTransAnestesicosFacade.java
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/RegistrosTransAnestesicosFacade.java
@@ -5,7 +5,7 @@
  */
 package ec.mil.he1.pom_04_portalservicios.beanpackage;
 
-import ec.mil.he1.pom_01_domain.RegistrosDeCierre;
+import ec.mil.he1.pom_01_domain.RegistrosTransAnestesicos;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,7 +15,7 @@ import javax.persistence.PersistenceContext;
  * @author christian_ruiz
  */
 @Stateless
-public class RegistrosDeCierreFacade extends AbstractFacade<RegistrosDeCierre> {
+public class RegistrosTransAnestesicosFacade extends AbstractFacade<RegistrosTransAnestesicos> {
     @PersistenceContext(unitName = "PU-WEB-PORTAL")
     private EntityManager em;
 
@@ -24,8 +24,8 @@ public class RegistrosDeCierreFacade extends AbstractFacade<RegistrosDeCierre> {
         return em;
     }
 
-    public RegistrosDeCierreFacade() {
-        super(RegistrosDeCierre.class);
+    public RegistrosTransAnestesicosFacade() {
+        super(RegistrosTransAnestesicos.class);
     }
     
 }
--- POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/RegulacionesActivosFijosFacade.java
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/RegulacionesActivosFijosFacade.java
@@ -5,8 +5,6 @@
  */
 package ec.mil.he1.pom_04_portalservicios.beanpackage;
 
-import ec.mil.he1.pom_01_domain.RegistrosDeCierre;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -14,18 +12,13 @@ import javax.persistence.PersistenceContext;
  *
  * @author christian_ruiz
  */
-@Stateless
-public class RegistrosDeCierreFacade extends AbstractFacade<RegistrosDeCierre> {
+public class RegulacionesActivosFijosFacade {
     @PersistenceContext(unitName = "PU-WEB-PORTAL")
     private EntityManager em;
 
     @Override
     protected EntityManager getEntityManager() {
         return em;
-    }
-
-    public RegistrosDeCierreFacade() {
-        super(RegistrosDeCierre.class);
     }
     
 }
