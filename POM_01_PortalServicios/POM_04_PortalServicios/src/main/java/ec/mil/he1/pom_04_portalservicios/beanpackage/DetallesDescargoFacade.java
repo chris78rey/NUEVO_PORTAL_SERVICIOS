644c4cf7e8fa301a6ffc71a62b9ed48a6570aaa3
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-11 11:37:25 -0500

    SE AGREGA ELSCHEMA

---
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/beanpackage/DetallesDescargoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.mil.he1.pom_04_portalservicios.beanpackage;
+
+import ec.mil.he1.pom_01_domain.DetallesDescargo;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author christian_ruiz
+ */
+@Stateless
+public class DetallesDescargoFacade extends AbstractFacade<DetallesDescargo> {
+    @PersistenceContext(unitName = "PU-WEB-PORTAL")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public DetallesDescargoFacade() {
+        super(DetallesDescargo.class);
+    }
+    
+}
