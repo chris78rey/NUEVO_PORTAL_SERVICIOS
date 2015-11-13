615f07214754c5587f6eaa9f3046135818a9471b
christian_ruiz <christian_ruiz@DES11.hg1.com>
2015-11-13 13:51:02 -0500

    listo ya funco controllers

--- POM_01_PortalServicios/POM_03_EJB/src/main/java/ec/mil/he1/pom_03_ejb/stateless/VUsuariosClasifFacade.java
+++ POM_01_PortalServicios/POM_03_EJB/src/main/java/ec/mil/he1/pom_03_ejb/stateless/VUsuariosClasifFacade.java
@@ -6,6 +6,7 @@
 package ec.mil.he1.pom_03_ejb.stateless;
 
 import ec.mil.he1.pom_01_domain.VUsuariosClasif;
+import javax.annotation.security.PermitAll;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -15,6 +16,7 @@ import javax.persistence.PersistenceContext;
  * @author christian_ruiz
  */
 @Stateless
+@PermitAll
 public class VUsuariosClasifFacade extends AbstractFacade<VUsuariosClasif> implements ec.mil.he1.pom_03_ejb.stateless.VUsuariosClasifFacadeRemote {
     @PersistenceContext(unitName = "PU-EJBPORTAL")
     private EntityManager em;
--- POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/controllers/LoginController.java
+++ POM_01_PortalServicios/POM_04_PortalServicios/src/main/java/ec/mil/he1/pom_04_portalservicios/controllers/LoginController.java
@@ -6,6 +6,8 @@
 package ec.mil.he1.pom_04_portalservicios.controllers;
 
 import ec.mil.he1.pom_01_domain.SegUsuario;
+import ec.mil.he1.pom_01_domain.VUsuariosClasif;
+import ec.mil.he1.pom_03_ejb.stateless.VUsuariosClasifFacadeRemote;
 import ec.mil.he1.pom_03_ejb.stateless.procesos.LoginSessionBeanRemote;
 import java.io.IOException;
 import javax.inject.Named;
@@ -25,47 +27,51 @@ import javax.naming.NamingException;
 @Named(value = "loginController")
 @SessionScoped
 public class LoginController implements Serializable {
-    
+
+    String paginaSiguiente = "";
+    @EJB
+    private transient VUsuariosClasifFacadeRemote vUsuariosClasifFacade;
+    VUsuariosClasif vUsuariosClasif = new VUsuariosClasif();
+
     private static final long serialVersionUID = -9036861759497150346L;
-    
     private SegUsuario segUsuario = new SegUsuario();
-    
+
     String mensaje = "";
-    
+
     @EJB
     private transient LoginSessionBeanRemote loginSessionBean;
-    
+
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public String getUsername() {
         return username;
     }
-    
+
     public void setUsername(String username) {
         this.username = username;
     }
     private String password = "";
     private String username = "";
     private String email = "";
-    
+
     public LoginSessionBeanRemote getLoginSessionBean() {
         return loginSessionBean;
     }
-    
+
     public void setLoginSessionBean(LoginSessionBeanRemote loginSessionBean) {
         this.loginSessionBean = loginSessionBean;
     }
-    
+
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -75,14 +81,17 @@ public class LoginController implements Serializable {
      */
     public LoginController() {
     }
-    
+
     public void buttonActionPersonal(ActionEvent actionEvent) throws SQLException {
         String Login = loginSessionBean.Login(username, password, "2");
-        
+
         switch (Login) {
             case "1":
                 mensaje = "Su usuario y clave estan correctas";
                 segUsuario = loginSessionBean.usuarioByCC(this.username);
+                //con este objeto se tiene ya los nombres
+                vUsuariosClasif = vUsuariosClasifFacade.find(segUsuario.getId());
+                paginaSiguiente = "dashboard.xhtml";
                 break;
             case "0":
                 mensaje = "Usuario o clave mal ingresados";
@@ -98,15 +107,15 @@ public class LoginController implements Serializable {
                 break;
             default:
                 mensaje = "";
-            
+
         }
-        
+
     }
-    
+
     public String accionIngresoPersonal() throws IOException, NamingException, SQLException {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Warning!", mensaje));
         mensaje = "";
-        return null;
+        return paginaSiguiente;
     }
 
     /**
@@ -122,5 +131,5 @@ public class LoginController implements Serializable {
     public void setSegUsuario(SegUsuario segUsuario) {
         this.segUsuario = segUsuario;
     }
-    
+
 }
